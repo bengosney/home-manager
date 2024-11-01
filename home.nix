d9ef7076e4eba56f57ab4201e958e7c804f75d12
Ben Gosney <bengosney@googlemail.com>
2024-11-01 07:53:24 +0000

    feat(NVM): add nvm support to bash
    I'm not massivly happy with this as it requires nvm to be installed manualy

--- home.nix
+++ home.nix
@@ -20,6 +20,10 @@
           unset -v have
           _have $1 && have=yes
       }
+
+      export NVM_DIR="$HOME/.nvm"
+      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+      [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
     '';
 
     shellAliases = {
