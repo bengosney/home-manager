01f519706be45656b4fc98fba240f71a39ff01d8
Ben Gosney <bengosney@googlemail.com>
2024-11-03 08:07:40 +0000

    feat(editor): set emacs as the default editor

--- home.nix
+++ home.nix
@@ -24,6 +24,7 @@
       export NVM_DIR="$HOME/.nvm"
       [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
       [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+      export EDITOR="emacs -nw"
     '';
 
     shellAliases = {
